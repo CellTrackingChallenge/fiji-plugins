03b03b6a8472d9437357374084aabd6cfa9a6c45
Vladimír Ulman <ulman@fi.muni.cz>
2022-06-22 12:37:34 +0200

    ADD: test/.../StartFiji to start Fiji from the IDE...

---
+++ src/test/java/net/celltrackingchallenge/fiji/plugins/StartFiji.java
@@ -0,0 +1,9 @@
+package net.celltrackingchallenge.fiji.plugins;
+
+import net.imagej.ImageJ;
+
+public class StartFiji {
+	public static void main(String[] args) {
+		new ImageJ().ui().showUI();
+	}
+}
